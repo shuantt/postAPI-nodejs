caef8aa700737bf63c702218e8700fd7e7e9643b
shuantt <tehsuan.tht@gmail.com>
2024-04-18 23:43:18 +0800

    feat: 修改成貼文版本

--- server.js
+++ server.js
@@ -4,109 +4,136 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const responseHandler = require('./responseHandler');
 //------------------- 取得環境變數 -------------------
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 //------------------- DB Setting -------------------
 const connectionStr = `${process.env.CONNECTION_STR}`
   .replace('<account>', process.env.DB_ACC)
   .replace('<password>', process.env.DB_PASSWORD);
 mongoose.connect(connectionStr);
-const Todo = require('./models/todo');
+const Post = require('./models/posts');
 //------------------- 請求監聽 -------------------
 const requestListener = async (request, response) => {
   let body = '';
   request.on('data', (chunk) => {
     body += chunk;
   });
-  if (request.url == '/todos' && request.method == 'GET') {
+  if (request.url == '/posts' && request.method == 'GET') {
     try {
-      const todos = await Todo.find({}).select(
-        '_id title completed tags createdAt updatedAt'
+      const posts = await Post.find({}).select(
+        '_id content image name likes createdAt updatedAt'
       );
-      responseHandler(response, 200, 'success', '取得全部代辦事項', todos);
+      responseHandler(response, 200, 'success', '取得全部貼文', posts);
     } catch (err) {
       console.log(err);
       responseHandler(response, 400, 'failed', '參數錯誤', []);
     }
-  } else if (request.url == '/todos' && request.method == 'POST') {
+  } else if (request.url == '/posts' && request.method == 'POST') {
     request.on('end', async () => {
       try {
-        const title = JSON.parse(body).title;
-        const completed = JSON.parse(body).completed;
-        const tags = JSON.parse(body).tags;
-        if (title !== undefined && title === '') {
-          responseHandler(response, 400, 'failed', 'title 不可為空', []);
+        const content = JSON.parse(body).content;
+        const image = JSON.parse(body).image;
+        const name = JSON.parse(body).name;
+        const likes = JSON.parse(body).likes;
+        if (content === undefined || name === undefined) {
+          responseHandler(
+            response,
+            400,
+            'failed',
+            'content 和 name 為必填',
+            []
+          );
+          return;
+        } else if (content === '' || name === '') {
+          responseHandler(
+            response,
+            400,
+            'failed',
+            'content 和 name 為必填',
+            []
+          );
           return;
         }
-        const result = await Todo.create({
-          title: title,
-          completed: completed,
-          tags: tags,
-        });
-        responseHandler(response, 200, 'success', '新增代辦事項成功', result);
+        else{
+          const result = await Post.create({
+            content: content,
+            image: image,
+            name: name,
+            likes: likes,
+          });
+          responseHandler(response, 200, 'success', '新增貼文成功', result);
+        }
       } catch (err) {
         responseHandler(response, 400, 'failed', '參數錯誤', []);
       }
     });
-  } else if (request.url == '/todos' && request.method == 'DELETE') {
+  } else if (request.url == '/posts' && request.method == 'DELETE') {
     try {
-      const result = await Todo.deleteMany();
+      const result = await Post.deleteMany();
       responseHandler(
         response,
         200,
         'success',
-        `已刪除全部共 ${result.deletedCount} 項代辦`,
+        `已刪除全部共 ${result.deletedCount} 貼文`,
         []
       );
     } catch (err) {
       responseHandler(response, 400, 'failed', '參數錯誤', []);
     }
-  } else if (request.url.startsWith('/todos/') && request.method == 'DELETE') {
+  } else if (request.url.startsWith('/posts/') && request.method == 'DELETE') {
     const id = request.url.split('/').pop();
     try {
-      const result = await Todo.findByIdAndDelete(id);
+      const result = await Post.findByIdAndDelete(id);
       if (result === null) {
-        responseHandler(response, 400, 'failed', '無此代辦 id', []);
+        responseHandler(response, 400, 'failed', '無此 id', []);
       } else {
         responseHandler(
           response,
           200,
           'success',
-          `代辦事項 ${result._id} 已刪除`,
+          `貼文 ${result._id} 已刪除`,
           result
         );
       }
     } catch (err) {
-      responseHandler(response, 400, 'failed', '參數錯誤或無此代辦 id', []);
+      responseHandler(response, 400, 'failed', '參數錯誤或無此 id', []);
     }
-  } else if (request.url.startsWith('/todos/') && request.method == 'PATCH') {
+  } else if (request.url.startsWith('/posts/') && request.method == 'PATCH') {
     request.on('end', async () => {
       try {
         const id = request.url.split('/').pop();
-        const title = JSON.parse(body).title;
-        const tags = JSON.parse(body).tags;
-        const completed = JSON.parse(body).completed;
-        if (title !== undefined && title === '') {
-          responseHandler(response, 400, 'failed', 'title 不可為空', []);
-          return;
-        }
-        const result = await Todo.findByIdAndUpdate(
-          id,
-          { title: title, completed: completed, tags: tags },
-          { returnDocument:'after' }
-        );
-        if (result === null) {
-          responseHandler(response, 400, 'failed', '無此代辦 id', []);
-        } else {
+        const content = JSON.parse(body).content;
+        const name = JSON.parse(body).name;
+        const image = JSON.parse(body).image;
+        const likes = JSON.parse(body).likes;
+        if (content === '' || name === '') {
           responseHandler(
             response,
-            200,
-            'success',
-            `代辦事項 ${result._id} 已更新`,
-            result
+            400,
+            'failed',
+            'content 和 name 不可為空',
+            []
           );
+          return;
+        } else {
+          const result = await Post.findByIdAndUpdate(
+            id,
+            { content: content, name: name, image: image, likes: likes },
+            { returnDocument: 'after' }
+          );
+          if (result === null) {
+            responseHandler(response, 400, 'failed', '無此 id', []);
+          } else {
+            responseHandler(
+              response,
+              200,
+              'success',
+              `貼文 ${result._id} 已更新`,
+              result
+            );
+          }
         }
       } catch (err) {
-        responseHandler(response, 400, 'failed', '參數錯誤或無此代辦 id', []);
+        responseHandler(response, 400, 'failed', '參數錯誤或無此 id', []);
       }
     });
   } else if (request.method == 'OPTIONS') {
